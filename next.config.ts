cec420ebde68870c968d109d0bfe14603a20579f
Matthias Meister <meister.matthias86@gmail.com>
2026-04-29 07:22:39 +0200

    Update Sentry configuration to use static organization and project names for consistency

--- next.config.ts
+++ next.config.ts
@@ -33,9 +33,9 @@ export default withSentryConfig(withNextIntl(nextConfig), {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: process.env.SENTRY_ORG ?? "lemonspace",
+  org: "lemonspace",
 
-  project: process.env.SENTRY_PROJECT ?? "lemonspace",
+  project: "lemonspace",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
